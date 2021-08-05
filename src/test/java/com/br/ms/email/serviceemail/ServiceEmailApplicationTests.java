94ad991595aff6a480a700c4cd5746309bd74fac
Developerheart <62905562+Developerheart@users.noreply.github.com>
2021-08-05 15:58:27 -0300

    First Commit

---
+++ src/test/java/com/br/ms/email/serviceemail/ServiceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ms.email.serviceemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
